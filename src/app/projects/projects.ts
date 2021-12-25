40d2986d3d2d35f51b78409570a3f55150b4a123
lakhoune <a.b.lakhoune@gmail.com>
2021-12-25 15:12:26 +0100

    add project

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -2,6 +2,19 @@ import { Project } from '../models/project.model';
 
 export const projects: Project[] = [
   {
+    title: 'Social Bot Framework',
+    subtitle: 'Bot modeling and development',
+    previewImgSrc: 'assets/sbf-preview.png',
+    page: {
+      href: 'https://sbf-dev.tech4comp.dbis.rwth-aachen.de',
+      description:
+        'A framework for building bots for social media platforms. It allows for intuitive approach to bot development',
+    },
+    source: {
+      href: 'https://github.com/rwth-acis/Social-Bot-Framework',
+    },
+  },
+  {
     title: 'MobSOS Evaluation Center',
     subtitle: 'Community Analytics',
     previewImgSrc: 'assets/mobsos-preview.png',
