46b28d606e3f2b00bd8458dda088e5356261a89b
lakhoune <a.b.lakhoune@gmail.com>
2021-12-29 17:33:17 +0100

    optimizations

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -4,7 +4,7 @@ export const projects: Project[] = [
   {
     title: 'Social Bot Framework',
     subtitle: 'Bot modeling and development',
-    previewImgSrc: 'assets/sbf-preview.png',
+    previewImgSrc: 'assets/sbf-preview.webp',
     page: {
       href: 'https://sbf-dev.tech4comp.dbis.rwth-aachen.de',
       description:
@@ -17,7 +17,7 @@ export const projects: Project[] = [
   {
     title: 'MobSOS Evaluation Center',
     subtitle: 'Community Analytics',
-    previewImgSrc: 'assets/mobsos-preview.png',
+    previewImgSrc: 'assets/mobsos-preview.webp',
     page: {
       href: 'https://sbf-dev.tech4comp.dbis.rwth-aachen.de/monitor/join/cb276112c6c7f5e1bbf11a4b136c6f44bcd36bcbb69ecb59274a4f71ee51eadd06cf3b6ca5e50ca154d8680e4d7a081265741f039da19c19f6dd176c82b03a87/i5.las2peer.services.tmitocar.TmitocarService/lakhoune',
       description:
@@ -30,7 +30,7 @@ export const projects: Project[] = [
   {
     title: 'StudyGrow Cards',
     subtitle: 'Digital Flashcards',
-    previewImgSrc: 'assets/cards-preview.png',
+    previewImgSrc: 'assets/cards-preview.webp',
     page: {
       href: 'https://studydrive.me',
       description:
