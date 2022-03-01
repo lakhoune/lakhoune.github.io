db661abe1f931213c776149ca782b206301818ca
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-03-01 22:18:56 +0100

    update studygrow project

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -32,7 +32,7 @@ export const projects: Project[] = [
     subtitle: 'Digital Flashcards',
     previewImgSrc: 'assets/cards-preview.webp',
     page: {
-      href: 'https://studydrive.me',
+      href: 'https://sleepy-cove-96643.herokuapp.com/',
       description:
         'This application can be used to create flashcards and share them with the community',
     },
