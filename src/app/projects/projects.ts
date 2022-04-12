c1b44449d3b7a1a8522ed59853375c6d119c5fcb
lakhoune <ben.lakhoune@rwth-aachen.de>
2022-04-12 15:43:01 +0200

    update link

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -19,7 +19,7 @@ export const projects: Project[] = [
     subtitle: 'Community Analytics',
     previewImgSrc: 'assets/mobsos-preview.webp',
     page: {
-      href: 'https://sbf-dev.tech4comp.dbis.rwth-aachen.de/monitor/join/70ed3c02f918ea91605a02e79c1fdde08e329ba1c2f64ddc556e5542aba40fbefdfa0f4aee8a9d80dc1942dce76d9ba93d667bc54bce4c0674eba5629701e391/i5.las2peer.services.mensaService.MensaService/lakhoune',
+      href: 'https://sbf-dev.tech4comp.dbis.rwth-aachen.de/monitor/join/63e2005b47421f5f94b0e8a841985d120e1ac814b57686a0570a66ec1df7d791f38f5628fc74a1cb93c819a89d5ba6fab4f355cf22ada3bb9f6a387473fabd6c/i5.las2peer.services.mensaService.MensaService/lakhoune',
       description:
         'MobSOS is a framework for community information systems (CIS) success modeling. The goal is to increase success awareness in a community.',
     },
