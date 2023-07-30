457f42b8447e50dea26ee83ec1d7dcc04e68dad7
lakhoune <a.b.lakhoune@gmail.com>
2023-07-30 18:46:05 +0200

    add social

--- src/app/socials/socials.ts
+++ src/app/socials/socials.ts
@@ -3,9 +3,10 @@ import { Social } from '../models/social.model';
 export const socials: Social[] = [
   new Social('bi bi-github', 'https://github.com/lakhoune'),
   new Social('bi bi-envelope-fill', 'mailto:a.b.lakhoune@gmail.com'),
+  new Social('bi bi-linkedin', 'https://www.linkedin.com/in/lakhoune/'),
   new Social(
-    'bi bi-linkedin',
-    'https://www.linkedin.com/in/lakhoune/'
+    'bi bi-patch-check-fill',
+    'https://www.credly.com/users/ben-lakhoune'
   ),
   new Social('bi bi-twitter', 'https://twitter.com/ben_lakhoune'),
 ];
