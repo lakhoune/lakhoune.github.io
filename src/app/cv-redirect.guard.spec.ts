3ca25cda22dd4eaae5fabc022e73844614a949dd
lakhoune <a.b.lakhoune@gmail.com>
2023-12-15 11:49:17 +0100

    try fix for test

--- src/app/cv-redirect.guard.spec.ts
+++ src/app/cv-redirect.guard.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 
-import { cvRedirectGuard } from './cv-redirect.guard';
+import { CvRedirectGuard } from './cv-redirect.guard';
 
 describe('cvRedirectGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => cvRedirectGuard(...guardParameters));
+  const executeGuard: CanActivateFn = (...guardParameters) =>
+    TestBed.runInInjectionContext(
+      () => new CvRedirectGuard(TestBed.inject(Router)) // Pass the Router instance
+    ).canActivate(...guardParameters);
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
