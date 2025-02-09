042f5a9f9dad30b6f3e7450a5adfd79b11274424
lakhoune <a.b.lakhoune@gmail.com>
2025-02-09 15:28:57 +0100

    migration

--- src/app/socials/socials.component.spec.ts
+++ src/app/socials/socials.component.spec.ts
@@ -1,7 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SocialsComponent } from './socials.component';
 import { socials } from './socials';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('SocialsComponent', () => {
   let component: SocialsComponent;
@@ -9,9 +10,10 @@ describe('SocialsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SocialsComponent],
-      imports: [HttpClientTestingModule],
-    }).compileComponents();
+    declarations: [SocialsComponent],
+    imports: [],
+    providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
+}).compileComponents();
   });
 
   beforeEach(() => {
