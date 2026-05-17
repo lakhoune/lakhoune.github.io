2183d48748041481b14717422a5faa0dddb50f76
lakhoune <a.b.lakhoune@gmail.com>
2026-05-17 20:47:28 +0200

    chore: checkpoint Angular 19 upgrade before optional migrations

--- src/app/projects/project/project.component.ts
+++ src/app/projects/project/project.component.ts
@@ -2,9 +2,10 @@ import { Component, Input, OnInit } from '@angular/core';
 import { EmptyProject, Project } from 'src/app/models/project.model';
 
 @Component({
-  selector: 'app-project',
-  templateUrl: './project.component.html',
-  styleUrls: ['./project.component.scss'],
+    selector: 'app-project',
+    templateUrl: './project.component.html',
+    styleUrls: ['./project.component.scss'],
+    standalone: false
 })
 export class ProjectComponent implements OnInit {
   @Input() project: Project = new EmptyProject();
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { projects } from './projects';
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss'],
+    selector: 'app-projects',
+    templateUrl: './projects.component.html',
+    styleUrls: ['./projects.component.scss'],
+    standalone: false
 })
 export class ProjectsComponent implements OnInit {
   projects = projects;
