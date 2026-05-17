55524deab044c3ff544a867327c08ea3bb0802c1
lakhoune <a.b.lakhoune@gmail.com>
2026-05-17 18:55:37 +0200

    konf fix for ci

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const isCI = !!process.env.CI;
 
 module.exports = function (config) {
   // Prefer Puppeteer's Chromium when available (works in CI and restricted macOS)
@@ -11,14 +12,14 @@ module.exports = function (config) {
   }
 
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
       jasmine: {
@@ -27,47 +28,49 @@ module.exports = function (config) {
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/website'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      dir: require("path").join(__dirname, "./coverage/website"),
+      subdir: ".",
+      reporters: [{ type: "html" }, { type: "text-summary" }],
     },
     files: [
       {
-        pattern: 'src/assets/images/mock-preview-img-1.jpg',
+        pattern: "src/assets/images/mock-preview-img-1.jpg",
         watched: false,
         included: false,
         served: true,
-        type: 'image/jpeg',
+        type: "image/jpeg",
         mime: {
-          'image/jpeg': ['jpg', 'jpeg'],
+          "image/jpeg": ["jpg", "jpeg"],
         },
       },
     ],
     proxies: {
-      '/assets/images/mock-preview-img-1.jpg': '/base/src/assets/images/mock-preview-img-1.jpg',
+      "/assets/images/mock-preview-img-1.jpg":
+        "/base/src/assets/images/mock-preview-img-1.jpg",
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ["progress", "kjhtml"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: (!!process.env.CI) ? ["ChromeHeadlessCI"] : ["ChromeHeadless"],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
-      }
+        base: "ChromeHeadless",
+        flags: [
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-dev-shm-usage",
+        ],
+      },
     },
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
   });
 };
